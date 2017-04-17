cc27fda76f35f14cebe2c5f361290b3e0759ccb0
Marcin Jakuszko <marcin.jakuszko@gmail.com>
2017-04-17 12:16:23 +0200

    April refresh, added polybar, rofi

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install i3
+sudo apt-get install feh
+sudo apt-get install git
+sudo apt-get install stow
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java
+
