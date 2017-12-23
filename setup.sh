2c76015a6bcd66d2f2f3b801a3f0d3e761ca4cad
Marcin Jakuszko <marcin.jakuszko@gmail.com>
2017-12-23 18:04:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt-get install i3
 sudo apt-get install feh
 sudo apt-get install git
 sudo apt-get install stow
+sudo apt-get install mc
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java
